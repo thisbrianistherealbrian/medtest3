35b4c050d1d8217519a66f486467e9ca2a0edf0f
Whizzy <hey@whizzy.dev>
2024-11-23 04:05:36 +0000

    Create database migrations

---
+++ database/migrations/0001_01_01_000004_create_documents_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('documents', function (Blueprint $table) {
+            $table->id();
+            $table->string('file_path');
+            $table->text('transcription')->nullable();
+            $table->string('text_based_pdf')->nullable();
+            $table->timestamps();
+            $table->foreignId('user_id')->nullable()->constrained()->cascadeOnUpdate()->nullOnDelete();
+            $table->foreignId('project_id')->nullable()->constrained()->cascadeOnUpdate()->nullOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('documents');
+    }
+};
