cb67f2a68c523c86bc42a660ab26786638575b32
Whizzy <hey@whizzy.dev>
2024-11-23 04:05:39 +0000

    Create DocumentResource in app panel

---
+++ app/Filament/App/Resources/DocumentResource.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Filament\App\Resources;
+
+use App\Filament\App\Resources\DocumentResource\Pages;
+use App\Models\Document;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Infolists;
+use Filament\Infolists\Infolist;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+
+class DocumentResource extends Resource
+{
+    protected static ?string $model = Document::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('file_path')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\Textarea::make('transcription')
+                    ->maxLength(65535)
+                    ->columnSpan('full'),
+                Forms\Components\TextInput::make('text_based_pdf')
+                    ->maxLength(255),
+                Forms\Components\Select::make('user_id')
+                    ->relationship(name: 'user', titleAttribute: 'name')
+                    ->searchable(),
+                Forms\Components\Select::make('project_id')
+                    ->relationship(name: 'project', titleAttribute: 'name')
+                    ->searchable(),
+            ]);
+    }
+
+    public static function infolist(Infolist $infolist): Infolist
+    {
+        return $infolist
+            ->schema([
+                Infolists\Components\TextEntry::make('file_path'),
+                Infolists\Components\TextEntry::make('transcription')
+                    ->columnSpan('full'),
+                Infolists\Components\TextEntry::make('text_based_pdf'),
+                Infolists\Components\TextEntry::make('created_at')
+                    ->dateTime(),
+                Infolists\Components\TextEntry::make('updated_at')
+                    ->dateTime(),
+                Infolists\Components\TextEntry::make('user.name'),
+                Infolists\Components\TextEntry::make('project.name'),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('file_path')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('text_based_pdf')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('user.name')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('project.name')
+                    ->searchable(),
+            ])
+            ->actions([
+                Tables\Actions\ViewAction::make(),
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListDocuments::route('/'),
+            'create' => Pages\CreateDocument::route('/create'),
+            'view' => Pages\ViewDocument::route('/{record}'),
+            'edit' => Pages\EditDocument::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ app/Filament/App/Resources/DocumentResource/Pages/CreateDocument.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\App\Resources\DocumentResource\Pages;
+
+use App\Filament\App\Resources\DocumentResource;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateDocument extends CreateRecord
+{
+    protected static string $resource = DocumentResource::class;
+}
