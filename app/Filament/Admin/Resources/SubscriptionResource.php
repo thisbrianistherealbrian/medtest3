a026f87e34cd8625c5616ffd26c67eaa8c9e6c7c
Whizzy <hey@whizzy.dev>
2024-11-23 04:05:39 +0000

    Create SubscriptionResource in admin panel

---
+++ app/Filament/Admin/Resources/SubscriptionResource.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Filament\Admin\Resources;
+
+use App\Filament\Admin\Resources\SubscriptionResource\Pages;
+use App\Models\Subscription;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Infolists;
+use Filament\Infolists\Infolist;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+
+class SubscriptionResource extends Resource
+{
+    protected static ?string $model = Subscription::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('level')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('price')
+                    ->numeric()
+                    ->required(),
+                Forms\Components\TextInput::make('max_projects')
+                    ->numeric()
+                    ->required(),
+                Forms\Components\Select::make('user_id')
+                    ->relationship(name: 'user', titleAttribute: 'name')
+                    ->searchable()
+                    ->required(),
+            ]);
+    }
+
+    public static function infolist(Infolist $infolist): Infolist
+    {
+        return $infolist
+            ->schema([
+                Infolists\Components\TextEntry::make('level'),
+                Infolists\Components\TextEntry::make('price')
+                    ->numeric(),
+                Infolists\Components\TextEntry::make('max_projects')
+                    ->numeric(),
+                Infolists\Components\TextEntry::make('created_at')
+                    ->dateTime(),
+                Infolists\Components\TextEntry::make('updated_at')
+                    ->dateTime(),
+                Infolists\Components\TextEntry::make('user.name'),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('level')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('price')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('max_projects')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('user.name')
+                    ->searchable(),
+            ])
+            ->actions([
+                Tables\Actions\ViewAction::make(),
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListSubscriptions::route('/'),
+            'create' => Pages\CreateSubscription::route('/create'),
+            'view' => Pages\ViewSubscription::route('/{record}'),
+            'edit' => Pages\EditSubscription::route('/{record}/edit'),
+        ];
+    }
+}
