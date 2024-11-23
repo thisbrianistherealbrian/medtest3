fcf30b71e057cfa05d1624e95eb08aa7d2e35361
Whizzy <hey@whizzy.dev>
2024-11-23 04:05:39 +0000

    Create DocumentResource in admin panel

---
+++ app/Filament/Admin/Resources/DocumentResource/Pages/CreateDocument.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Admin\Resources\DocumentResource\Pages;
+
+use App\Filament\Admin\Resources\DocumentResource;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateDocument extends CreateRecord
+{
+    protected static string $resource = DocumentResource::class;
+}
