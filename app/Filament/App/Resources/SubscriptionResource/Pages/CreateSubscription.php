f7ec726fd91535d9ccdca7b1954173c8cc5d97b9
Whizzy <hey@whizzy.dev>
2024-11-23 04:05:39 +0000

    Create SubscriptionResource in app panel

---
+++ app/Filament/App/Resources/SubscriptionResource/Pages/CreateSubscription.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\App\Resources\SubscriptionResource\Pages;
+
+use App\Filament\App\Resources\SubscriptionResource;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateSubscription extends CreateRecord
+{
+    protected static string $resource = SubscriptionResource::class;
+}
